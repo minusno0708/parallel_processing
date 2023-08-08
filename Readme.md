6f413d4c4daed777e3c73a8fb43749eaea8bf9d5
minusno0708 <naoyao1014@gmail.com>
2023-08-08 19:37:17 +0900

    pythonでの並列処理の実装

---
+++ Readme.md
@@ -0,0 +1,3 @@
+**GO TechTalk #21 の実装リポジトリ**
+
+並行処理をGO,Rust,Kotlin,Python,JSで実装する
---
+++ python/sample.py
@@ -0,0 +1,16 @@
+from concurrent.futures import ProcessPoolExecutor
+from random import random
+from time import sleep
+
+
+def func(key):
+    sleep(random())
+    print(key)
+
+def do_parallel():
+    with ProcessPoolExecutor() as executor:
+        for key in [1, 2, 3]:
+            executor.submit(func, key)
+
+if __name__ == '__main__':
+    do_parallel()
